ff017b85f7c0cb6d792a916fc8e543a0073fbf4c
choniyuazwan <choniyuazwan@gmail.com>
2019-05-02 21:56:15 +0700

    add post artist

--- app.js
+++ app.js
@@ -1,10 +1,52 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const Playlist = require('./models/playlist')
+const Artist = require('./models/artist')
+const Album = require('./models/album')
+const Track = require('./models/track')
 const Sequelize = require('sequelize')
 
 const {Op} = Sequelize
-
 const app = express()
+
+app.use(bodyParser.json())
+
+Artist.hasMany(Album, {
+    foreignKey: 'ArtistId'
+})
+
+Album.belongsTo(Artist, {
+    foreignKey: 'ArtistId'
+})
+
+Playlist.belongsToMany(Track, {
+    through: 'playlist_track',
+    foreignKey: 'PlaylistId',
+    timestamps: false
+})
+
+Track.belongsToMany(Playlist, {
+    through: 'playlist_track',
+    foreignKey: 'TrackId',
+    timestamps: false
+})
+
+app.post('/api/artists', (request, response) => {
+    Artist.create({
+        name: request.body.name
+    }).then((artist) => {
+        response.json(artist)
+    }, (validation) => {
+        response.status(402).json({
+            errors: validation.errors.map((error) => {
+                return {
+                    attribute: error.path,
+                    message: error.message
+                }
+            })
+        })
+    })
+})
 
 app.get('/api/playlists', (request, response) => {
     let filter = {}
@@ -28,9 +70,53 @@ app.get('/api/playlists', (request, response) => {
 app.get('/api/playlists/:id', (request, response) => {
     let {id} = request.params
 
-    Playlist.findByPk(id).then((playlist) => {
+    Playlist.findByPk(id, {
+        include: [Track]
+    }).then((playlist) => {
         if(playlist) {
             response.json(playlist)
+        } else {
+            response.status(404).send()
+        }
+    })
+});
+
+app.get('/api/tracks/:id', (request, response) => {
+    let {id} = request.params
+
+    Track.findByPk(id, {
+        include: [Playlist]
+    }).then((track) => {
+        if(track) {
+            response.json(track)
+        } else {
+            response.status(404).send()
+        }
+    })
+});
+
+app.get('/api/artists/:id', (request, response) => {
+    let {id} = request.params
+
+    Artist.findByPk(id, {
+        include: [Album]
+    }).then((artist) => {
+        if(artist) {
+            response.json(artist)
+        } else {
+            response.status(404).send()
+        }
+    })
+});
+
+app.get('/api/albums/:id', (request, response) => {
+    let {id} = request.params
+
+    Album.findByPk(id, {
+        include: [Artist]
+    }).then((album) => {
+        if(album) {
+            response.json(album)
         } else {
             response.status(404).send()
         }
